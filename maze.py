0b8df1ca3fa0ced40334c3d85fffeaf974cf879e
Szymon Iwacz <szymon@iwacz.pl>
2024-03-17 09:13:12 +0100

    Use StackFrontier to solve the maze

--- maze.py
+++ maze.py
@@ -127,6 +127,7 @@ class Maze():
         # Initialize frontier to just the starting position
         start = Node(state=self.start, parent=None, action=None)
         frontier = StackFrontier()
+        print("Using stack frontier.")
         frontier.add(start)
 
         # Initialize an empty explored set
